c954d95e9fc61a9745271d8ca6fe1aa2b14e9493
Nikolaus Krondraf <krondraf@technikum-wien.at>
2016-03-30 10:05:38 +0200

    Fehler in Übersetzung behoben

--- locale/it-IT/tools.php
+++ locale/it-IT/tools.php
@@ -3,6 +3,6 @@
 $this->phrasen['tools/dokumente']='Documenti';
 $this->phrasen['tools/studienbeitragFuerSJNochNichtBezahlt']='Tasse per il anno accademico %1$s non ancora versate';
 $this->phrasen['tools/inskriptionsbestaetigung']='Certificato d\'iscrizione';
-$this->phrasen['tools/studienbeitragFuerSJBezahlt']='Tasse per il anno accademico %1$s versate';
+$this->phrasen['tools/studienbeitragFuerSJBezahlt']='Tasse per l\'anno accademico %1$s versate';
 
 ?>
