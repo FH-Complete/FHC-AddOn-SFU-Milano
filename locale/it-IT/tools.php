d1443db194f239ec75b269369a16e5707d1ad6d2
Gerald Raab <raab@technikum-wien.at>
2016-05-04 15:01:09 +0200

    phrases

--- locale/it-IT/tools.php
+++ locale/it-IT/tools.php
@@ -1,8 +1,8 @@
 <?php
 
-$this->phrasen['tools/dokumente']='Documenti';
+$this->phrasen['tools/dokumente']='Dichiarazione sostitutiva di certificazione';
 $this->phrasen['tools/studienbeitragFuerSJNochNichtBezahlt']='Tasse per l\'anno accademico %1$s non ancora versate';
-$this->phrasen['tools/inskriptionsbestaetigung']='Certificato d\'iscrizione';
+$this->phrasen['tools/inskriptionsbestaetigung']='Dichiarazione d\'iscrizione';
 $this->phrasen['tools/studienbeitragFuerSJBezahlt']='Tasse per l\'anno accademico %1$s versate';
 
 ?>
