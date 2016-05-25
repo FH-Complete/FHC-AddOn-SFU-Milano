c225f12de6aafe5595d531a674db1e152fb90d30
Gerald Raab <raab@technikum-wien.at>
2016-05-25 14:36:17 +0200

    it

--- locale/it-IT/bewerbung.php
+++ locale/it-IT/bewerbung.php
@@ -3,5 +3,5 @@
 $this->phrasen['bewerbung/bewerbungsstatus']=' ';
 $this->phrasen['bewerbung/bestaetigt']=' ';
 $this->phrasen['bewerbung/nichtBestaetigt']=' ';
-$this->phrasen['bewerbung/welcome']='Benvenuto alla prenotazione del colloquio di selezione<br><br><p style="font-size:11pt;">Alla Sigmund Freud University di Milano la selezione dei partecipanti ai corsi di laurea avviene mediante un colloquio individuale.<br><br>In quest’area è possibile registrarsi al fine di sostenere il colloquio per l’ammissione al corso di Laurea Triennale o Magistrale in Psicologia.</p>';
+$this->phrasen['bewerbung/welcome']='Benvenuto alla prenotazione del colloquio di selezione<br><br><p style="font-size:11pt; text-align:left;">Alla Sigmund Freud University di Milano la selezione dei partecipanti ai corsi di laurea avviene mediante un colloquio individuale.<br><br>In quest’area è possibile registrarsi al fine di sostenere il colloquio per l’ammissione al corso di Laurea Triennale o Magistrale in Psicologia.</p>';
 ?>
