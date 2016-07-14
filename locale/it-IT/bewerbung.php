4260d8032233157c2a51f31265fd1418a64128e8
Gerald Raab <raab@technikum-wien.at>
2016-07-14 18:04:15 +0200

    footer-phrase on bewerbung registration page

--- locale/it-IT/bewerbung.php
+++ locale/it-IT/bewerbung.php
@@ -4,4 +4,19 @@ $this->phrasen['bewerbung/bewerbungsstatus']=' ';
 $this->phrasen['bewerbung/bestaetigt']=' ';
 $this->phrasen['bewerbung/nichtBestaetigt']=' ';
 $this->phrasen['bewerbung/welcome']='Benvenuto alla prenotazione del colloquio di selezione<br><br><p style="font-size:11pt; text-align:left;">Alla Sigmund Freud University di Milano la selezione dei partecipanti ai corsi di laurea avviene mediante un colloquio individuale.<br><br>In quest’area è possibile registrarsi al fine di sostenere il colloquio per l’ammissione al corso di Laurea Triennale o Magistrale in Psicologia.</p>';
+$this->phrasen['bewerbung/footerText']='
+<hr>
+<table>
+<tr>
+<td><a href="http://milano-sfu.it">Sigmund Freud Privat Universität Wien GmbH</a></td>
+</tr>
+<tr><td>
+Sede Italiana : Via Filippo Argelati, 40 20143 Milano<br>
+Ingresso e Casella Postale : Ripa di Porta Ticinese, 77 20143 Milano<br>
+P.IVA : 09088530960<br>
+C.F. : 97672050156<br>
+REA : MI-2025093
+</td>
+</tr></table><br><br>
+';
 ?>
