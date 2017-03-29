85865da9a04b78b2d90fc46b85c14c96cc6def3b
Gerald Raab <raab@technikum-wien.at>
2017-03-29 18:09:27 +0200

    Changed Phrase

--- locale/it-IT/studienplan.php
+++ locale/it-IT/studienplan.php
@@ -1,5 +1,5 @@
 <?php
-$this->phrasen['studienplan/Anmeldung']='Scelta del corso opzionale (non selezionare se già iscritto ad altro corso opzionale)';
+$this->phrasen['studienplan/Anmeldung']='Scelta del corso opzionale (non selezionare se già iscritto ad altro corso opzionale equivalente)';
 $this->phrasen['studienplan/legendeAngemeldet']='iscritto al corso opzionale';
 
 ?>
