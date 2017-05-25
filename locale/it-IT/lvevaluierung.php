32fe71fd63c2227667e0bb1b27fb08cae6fb5324
Gerry Raab <raab@technikum-wien.at>
2017-05-25 23:17:28 +0200

    ital. translations

--- locale/it-IT/lvevaluierung.php
+++ locale/it-IT/lvevaluierung.php
@@ -1,13 +1,8 @@
 <?php
 
-$this->phrasen['lvevaluierung/einleitungstext']='<h2>Scheda di valutazione corsi</h2>
-Il sondaggio serve per la valutazione dei corsi PS/SE/UE/VO<br>
-Il questionario è anonimo e le informazioni fornite servono per valutare la qualità interna<br>
-L’indagine è divisa in aree tematiche che possono essere valutate sulla base di una scala da  1 a 6 come segue:<br>
-1=insoddisfacente  2=Mediocre  3=Sufficiente  4=Discreto   5= Buono  6=Molto buono <br>
-Alla fine del sondaggio è possibile lasciare commenti personali. Si richiede di leggere attentamente le istruzioni e di fornire le informazioni il più spontaneo possibile. <br>
-Il tempo di elaborazione previsto è di circa 5 minuti. Grazie e buon divertimento!<br>
-<br>Inserire il codice per effettuare la valutazione del corso
+$this->phrasen['lvevaluierung/einleitungstext']='Per effettuare la valutazione di un corso inserire un codice valido.<br><br>
+I codici per la valutazione di un corso sono distribuiti al termine del corso in forma cartacea e in maniera casuale agli studenti. Ciascun codice è valido per una sola volta.<br>
+Il presente sistema garantisce l’anonimato.
 ';
 
 
