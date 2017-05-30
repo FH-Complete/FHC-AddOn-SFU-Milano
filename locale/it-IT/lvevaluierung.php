53ba222d109204285022ac3a90b65047bd814374
Gerald Raab <raab@technikum-wien.at>
2017-05-30 12:56:46 +0200

    Templates for LVE

--- locale/it-IT/lvevaluierung.php
+++ locale/it-IT/lvevaluierung.php
@@ -4,6 +4,6 @@ $this->phrasen['lvevaluierung/einleitungstext']='Per effettuare la valutazione d
 I codici per la valutazione di un corso sono distribuiti al termine del corso in forma cartacea e in maniera casuale agli studenti. Ciascun codice è valido per una sola volta.<br>
 Il presente sistema garantisce l’anonimato.
 ';
-
+$this->phrasen['lvevaluierung/infotextAllgemein']='';
 
 ?>
